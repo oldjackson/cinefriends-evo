2216e6f7befec049440a87e138ce2d4fd0f8e5a8
Lorenzo Fermi <Lorenzofermi@gmail.com>
2018-03-06 18:09:49 +0000

    Fix broken migrations and fix navbar in place

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306174833) do
+ActiveRecord::Schema.define(version: 20180306125357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,10 +118,10 @@ ActiveRecord::Schema.define(version: 20180306174833) do
 
   create_table "theaters", force: :cascade do |t|
     t.string "name"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
-    t.string "location"
   end
 
   create_table "users", force: :cascade do |t|
