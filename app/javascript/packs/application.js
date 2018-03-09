4687a6e6042bb0792eea7e4f202157518be5b26a
Joao Sanches <joao.carrelo.sanches@gmail.com>
2018-03-09 13:42:24 +0000

    alert fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+
+$(document).ready(function(){
+
+// After 3 seconds the alerts are vanishing
+  setTimeout(
+    function() {
+      $('.alert').slideUp();
+  }, 2000);
+
+
+})
